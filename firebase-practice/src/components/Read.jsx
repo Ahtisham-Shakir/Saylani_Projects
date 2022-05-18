713962abc57e03c88dfce6ad643228ed95aded3b
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-05-19 00:51:30 +0500

    Added Delete user functionality

--- firebase-practice/src/components/Read.jsx
+++ firebase-practice/src/components/Read.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { firestore } from '../config/firebase'
-import { collection, getDocs } from 'firebase/firestore/lite'
+import { collection, deleteDoc, getDocs, doc } from 'firebase/firestore/lite'
+import { useGlobalContext } from '../context';
 
 const Read = () => {
     const [documents, setDocuments] = useState([]);
@@ -8,50 +9,73 @@ const Read = () => {
     const collectionName = 'users'
     const collectionRef = collection(firestore, collectionName);
 
-    const readDocs = async()=>{
+    const { showAlert } = useGlobalContext();
+
+    const readDocs = async () => {
         let array = [];
 
         const querySnapshot = await getDocs(collectionRef);
         querySnapshot.forEach(docs => {
-            array.push({...docs.data(), id:docs.id});
+            array.push({ ...docs.data(), id: docs.id });
         });
         setDocuments(array);
         setIsLoading(false);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         readDocs();
-    },[])
-    
+    }, [])
+
+    // Function for deleting documents
+    const deleteDocument = async (document) => {
+        try {
+
+            await deleteDoc(doc(firestore, collectionName, document.id));
+
+            showAlert('success', 'User has been deleted successfully');
+
+            let newArray = documents.filter((doc) => {
+                return document.id !== doc.id;
+            })
+
+            setDocuments(newArray);
+
+        } catch (error) {
+            console.log(error);
+            showAlert('error', 'User is not deleted! Please Try Again.')
+        }
+
+    }
+
     return (
         <div className='d-flex justify-content-center align-items-center min-vh-100'>
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h1 className='text-center text-white'>Read Users</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h1 className='text-center text-white'>Read Users</h1>
+                    </div>
                 </div>
-            </div>
-            <div className="row mt-3">
-                <div className="col">
-                    {isLoading ?
-                        <div className="text-center">
-                            <div class="spinner-grow text-white" role="status">
-                                <span class="visually-hidden">Loading...</span>
+                <div className="row mt-3">
+                    <div className="col">
+                        {isLoading ?
+                            <div className="text-center">
+                                <div className="spinner-grow text-white" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </div>
                             </div>
-                        </div>
-                        : <>
-                            {
-                                documents.map((doc) => {
-                                    return <p key={doc.id} className="text-white text-center">FullName: {doc.name} || Age: {doc.age}</p>
-                                })
-                            }
-                        </>
-                    }
+                            : <>
+                                {
+                                    documents.map((doc) => {
+                                        return <p key={doc.id} className="text-white text-center" >FullName: {doc.name} || Age: {doc.age} <button className='btn btn-primary' onClick={() => deleteDocument(doc)}>delete</button></p>
+                                    })
+                                }
+                            </>
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
 
-    </div>
+        </div>
     )
 }
 
